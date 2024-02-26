23f027e6332f3f6733a9cc902089e97337eb36ea
Gmarques023 <153223582+Gmarques023@users.noreply.github.com>
2024-02-26 20:32:40 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,55 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateCard() {
+  const suits = ["heart", "spade", "club", "diamond"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  // Generate random index for suits and values arrays
+  const suitIndex = Math.floor(Math.random() * suits.length);
+  const valueIndex = Math.floor(Math.random() * values.length);
+
+  // Select the card element and set its class to the randomly selected suit
+  const card = document.getElementById("card");
+  card.className = "card " + suits[suitIndex];
+
+  // Set the content of the card to the randomly selected value
+  card.textContent = values[valueIndex];
+}
+
+function generateCardEvery10Seconds() {
+  generateCard();
+  setTimeout(generateCardEvery10Seconds, 1000);
+}
+
+function generateCardByDimensions() {
+  const widthInput = document.getElementById("input-width").value;
+  const heightInput = document.getElementById("input-height").value;
+  card.style.width = widthInput + "px";
+  card.style.height = heightInput + "px";
+}
+// Generate a random card when the page loads
+window.onload = generateCard;
+
+// Generate a Random Card Button Event
+document.getElementById("generateCard").addEventListener("click", generateCard);
+document
+  .getElementById("timerGenerator")
+  .addEventListener("click", generateCardEvery10Seconds);
+document
+  .getElementById("dimensions-button")
+  .addEventListener("click", generateCardByDimensions);
